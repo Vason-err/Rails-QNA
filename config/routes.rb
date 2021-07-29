311ad74b3b7f46f080e3e9fcb58d56d892455c76
Vason-err <vasya.vedan@gmail.com>
2021-07-29 20:29:10 +0300

    Install devise, add user model, add devise to user, gem launch, generate devise views and controllers for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'user/sessions'
+  }
+
+  root to: "home#index"
+
   resources :questions, only: [:show, :new, :create] do
     resources :answers, only: [:new, :create], shallow: true
   end
