febdce8e2436516e676cf5e134b96316e5c3d2da
Vason-err <vasya.vedan@gmail.com>
2021-08-06 00:34:26 +0300

    Add jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
 const { environment } = require('@rails/webpacker')
-
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        Jquery: 'jquery/srs/jquery'
+    })
+)
 module.exports = environment
