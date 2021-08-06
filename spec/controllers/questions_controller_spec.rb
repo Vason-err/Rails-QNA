f7819da662fd6e747b9dd300bab87008769b99e1
Vason-err <vasya.vedan@gmail.com>
2021-08-06 17:14:03 +0300

    Write down tests for questions controller for patch update and get index

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    let!(:questions) { create_list(:question, 3) }
+
+    before { get :index }
+
+    it 'assigns the requested questions to @questions' do
+      expect(assigns(:questions)).to eq(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+
   describe 'GET #show' do
     let(:question) { create(:question) }
 
     before { get :show, params: { id: question } }
 
     it 'assigns the requested question to @question' do
-      expect(assigns(:question)).to eq question
+      expect(assigns(:question)).to eq(question)
     end
 
     it 'renders show view' do
@@ -57,6 +71,32 @@ RSpec.describe QuestionsController, type: :controller do
           post_create
 
           expect(response).to render_template :new
+        end
+      end
+    end
+
+    describe 'PATCH #update' do
+      let(:patch_update) { patch :update, params: { id: question.id, question: { body: new_body } }, format: :js }
+
+      let!(:question) { create(:question, user: user) }
+      let(:body) { question.body }
+      let(:new_body) { 'new body' }
+
+      it 'updates the question in the database' do
+        expect { patch_update }.to change { question.reload.body }.from(body).to(new_body)
+      end
+
+      context 'with invalid attributes' do
+        let(:new_body) { nil }
+
+        it 'does not update the answer' do
+          expect { patch_update }.not_to change { question.body }
+        end
+      end
+
+      context 'when the user is not the author' do
+        it 'does not update the answer' do
+          expect { patch_update }.not_to change { question.body }
         end
       end
     end
