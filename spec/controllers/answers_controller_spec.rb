544e95dc0e8fffb8110b17369c9a5b9f6218ee86
Vason-err <vasya.vedan@gmail.com>
2021-08-07 19:58:31 +0300

    Refactor answers controllers specs(added new one, but forget to refactor old)

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -17,22 +17,12 @@ RSpec.describe AnswersController, type: :controller do
       it 'adds a answer to the user' do
         expect { post_create }.to change(user.answers, :count).by(1)
       end
-
-      it 'redirects to related question show view' do
-        post_create
-        expect(response).to redirect_to question
-      end
     end
 
     context 'with invalid attributes' do
       let(:answer_params) { attributes_for(:answer, :invalid) }
       it 'does not save the question' do
         expect { post_create }.to_not change(Answer, :count)
-      end
-
-      it 'renders question show view' do
-        post_create
-        expect(response).to render_template 'questions/show'
       end
     end
   end
@@ -69,12 +59,7 @@ RSpec.describe AnswersController, type: :controller do
     let!(:answer) { create(:answer, user: user, question: question) }
 
     it 'should delete answer' do
-      expect { delete_destroy }.to change(question.answers, :count).by(-1)
-    end
-
-    it 'redirects to question' do
-      delete_destroy
-      expect(response).to redirect_to question_path(question)
+      expect { delete_destroy}.to change(question.answers, :count).by(-1)
     end
 
     context 'when the user is not the author' do
@@ -83,41 +68,25 @@ RSpec.describe AnswersController, type: :controller do
       it 'should not delete answer' do
         expect { delete_destroy }.not_to change(question.answers, :count)
       end
-
-      it 'redirects to question' do
-        delete_destroy
-        expect(response).to redirect_to question_path(question)
-      end
     end
 
-    context 'when best answer' do
-      let!(:best_answer_id) { answer.id }
+  end
 
-      before do
-        answer.mark_as_best
-      end
+  describe 'POST #mark_as_best' do
+    let(:post_mark) { post :mark_as_best, params: { id: answer.id }, format: :js }
 
-      it 'should delete best answer' do
-        expect { subject }.to change { question.reload.best_answer_id }.from(best_answer_id).to(nil)
-      end
+    let(:question) { create(:question, user: user) }
+    let!(:answer) { create(:answer, question: question) }
+
+    it 'should marks answer as best' do
+      expect { post_mark }.to change { question.reload.best_answer_id }.from(nil).to(answer.id)
     end
 
-    describe 'POST #mark_as_best' do
-      let(:post_mark) { post :mark_as_best, params: { id: answer.id }, format: :js }
+    context 'when the user is not the question author' do
+      let(:question) { create(:question) }
 
-      let(:question) { create(:question, user: user) }
-      let!(:answer) { create(:answer, question: question) }
-
-      it 'should marks answer as best' do
-        expect { post_mark }.to change { question.reload.best_answer_id }.from(nil).to(answer.id)
-      end
-
-      context 'when the user is not the question author' do
-        let(:question) { create(:question) }
-
-        it 'should not marks answer as best' do
-          expect { post_mark }.not_to change { question.reload.best_answer_id }
-        end
+      it 'should not marks answer as best' do
+        expect { post_mark }.not_to change { question.reload.best_answer_id }
       end
     end
   end
