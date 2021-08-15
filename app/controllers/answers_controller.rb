cdbcc0dac2e7d16aa6433d013fc25eb2c9790ece
Vason-err <vasya.vedan@gmail.com>
2021-08-15 20:17:22 +0300

    Refactor answers and questions controllers, add files controller, update routes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -36,7 +36,7 @@ class AnswersController < ApplicationController
   private
 
   def answer_params
-    params.require(:answer).permit(:body)
+    params.require(:answer).permit(:body, files: [])
   end
 
   def find_question
@@ -44,7 +44,7 @@ class AnswersController < ApplicationController
   end
 
   def find_answer
-    @answer = Answer.find(params[:id])
+    @answer = Answer.with_attached_files.find(params[:id])
   end
 
   def check_answer_author
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  resources :files, only: [:destroy]
 end
