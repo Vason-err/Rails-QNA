5ba4c11c6d49c7feef4e7efc7b736c652309f5df
Vason-err <vasya.vedan@gmail.com>
2021-08-19 23:16:44 +0300

    Refactor links viewing, add url format for link

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Link, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+  it { should allow_values('http://foo.bar', 'https://foo.bar.com').for(:url) }
 end
