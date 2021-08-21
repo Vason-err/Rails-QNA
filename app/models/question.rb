1ed3160f244f6f01480a567633f2d52250363f24
Vason-err <vasya.vedan@gmail.com>
2021-08-21 17:17:21 +0300

    Define reward model, refactor question and user models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :user
-  has_many :answers, -> { order('best DESC, created_at') }, dependent: :destroy
 
-  has_many_attached :files
+  has_many :answers, -> { order('best DESC, created_at') }, dependent: :destroy
   has_many :links, dependent: :destroy, as: :linkable
+  has_many_attached :files
+  has_one :reward, dependent: :destroy
 
   validates :title, :body, presence: true
 
   accepts_nested_attributes_for :links, reject_if: :all_blank
+  accepts_nested_attributes_for :reward, reject_if: :all_blank
 end
