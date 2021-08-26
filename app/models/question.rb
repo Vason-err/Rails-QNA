ab8148dbd154c66425facfdc9a1a2c143e4ce124
Vason-err <vasya.vedan@gmail.com>
2021-08-26 20:31:34 +0300

    add voteble and linkable concerns, refactor answer and question models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
+  include Linkable
+  include Voteable
+
   belongs_to :user
 
   has_many :answers, -> { order('best DESC, created_at') }, dependent: :destroy
   has_many :links, dependent: :destroy, as: :linkable
   has_many_attached :files
   has_one :reward, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
   validates :title, :body, presence: true
 
-  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :reward, reject_if: :all_blank
 end
